62b9bdc5e4babdb4aaee9a64e59f75fc30b66ff9
Joako119 <joaquindeamelio400@gmail.com>
2023-07-10 21:08:50 -0300

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ejercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Double grados;
+            Double radianes;
+            Double resultado;
+            Console.WriteLine("ingresar el valor en grados");
+            grados= Double.Parse(Console.ReadLine());
+
+            if (grados > 0)
+            {
+                radianes = (0.0174532925);
+                resultado = grados * radianes;
+                Console.Write($"el valor en radianes es {resultado}");
+            }
+            else
+            {
+                Console.WriteLine("el valor debe ser mayor a 0.");
+            }
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
